87e1ed53a32af5393cdd0bf38636a2a0c5062f1c
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-09-12 17:34:22 -0400

    Create time.js

---
+++ js/time.js
@@ -0,0 +1,9 @@
+var update_loop = setInterval(Main, 1000);
+Main();
+function Main(){
+    var time = new Date();
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+    document.getElementById("time").innerHTML = hours + ':' + minutes + ':' + seconds;
+}
