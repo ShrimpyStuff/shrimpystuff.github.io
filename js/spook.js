8b6803f97183c8517c6b5805779215ab028b629a
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-09-30 19:09:37 -0400

    Create spook.js

---
+++ js/spook.js
@@ -0,0 +1,16 @@
+var update_loop = setInterval(Main, 1000);
+Main();
+function Main(){
+    var date = new Date();
+var now = new Date().getTime();
+var year = date.getYear();
+var countDownDate = new Date("Oct 1, 2020 0:0:0").getTime();
+var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+ document.getElementById("spook").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+}
