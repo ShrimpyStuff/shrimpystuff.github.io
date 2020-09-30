918990e1f98f88f694d98827538f54f4b559a59f
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-09-30 19:11:13 -0400

    Update spook.js

--- js/spook.js
+++ js/spook.js
@@ -11,6 +11,6 @@ var distance = countDownDate - now;
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
- document.getElementById("spook").innerHTML = days + "d " + hours + "h "
+ document.getElementById("Spook").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 }
