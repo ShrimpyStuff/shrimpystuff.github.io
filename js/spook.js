1d312856c086076a8a0c5edd2b02426fb585269b
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-01 09:15:46 -0400

    Update spook.js

--- js/spook.js
+++ js/spook.js
@@ -3,8 +3,8 @@ Main();
 function Main(){
     var date = new Date();
 var now = new Date().getTime();
-var year = date.getYear();
-var countDownDate = new Date("Oct 1, 2020 0:0:0").getTime();
+var month = date.getMonth();
+var countDownDate = new Date("Oct 1, 2021 0:0:0").getTime();
 var distance = countDownDate - now;
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -12,5 +12,10 @@ var distance = countDownDate - now;
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
  document.getElementById("Spook").innerHTML = "<h1>" + days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s " + "</h1>";
+  + minutes + "m " + seconds + "s " + " till Spooktober</h1>";
+    if (month == 9) {
+        document.getElementById("Spook").innerHTML = "<h1>It's Spooktober!!!</h1>";
+    } else if (distance < 0) {
+        document.getElementById("Spook").innerHTML = "<h1>This is expired please wait for me to update it.</h1>";
+    }
 }
