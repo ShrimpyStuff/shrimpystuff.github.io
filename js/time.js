ba8d981c8ff21b51883904416d0cbb55b3fe355b
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2021-02-01 12:34:34 -0500

    Delete time.js

--- js/time.js
+++
@@ -1,9 +0,0 @@
-var update_loop = setInterval(Main, 1000);
-Main();
-function Main(){
-    var time = new Date();
-    var hours = time.getHours();
-    var minutes = time.getMinutes();
-    var seconds = time.getSeconds();
-    document.getElementById("time").innerHTML =  "<h1>" + hours + ':' + minutes + ':' + seconds + "</h1>";
-}
