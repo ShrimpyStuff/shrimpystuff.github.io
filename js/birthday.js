ebcafd6171805872bdd0887384e98594475da8b9
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2021-09-30 10:58:27 -0400

    Update birthday.js

--- js/birthday.js
+++ js/birthday.js
@@ -3,6 +3,6 @@
   var day = date.getDate().toLocaleString("en-CA", {timeZone: "America/Toronto"});
   var month = date.getMonth().toLocaleString("en-CA", {timeZone: "America/Toronto"});
 if (day ==  17 && month == 9) {
-    Title.innerHTML = "IT'S MY BIRTHDAY";
+    Title.innerHTML = "IT'S MY BIRTHDAY | ShrimpyStuff";
     document.title = "IT'S MY BIRTHDAY | ShrimpyStuff | Home";
   }
