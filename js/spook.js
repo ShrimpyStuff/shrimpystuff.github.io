69595a62fd1526750d42f8c2c6fea86cf1168f32
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2021-11-19 11:32:28 -0500

    Update spook.js

--- js/spook.js
+++ js/spook.js
@@ -2,25 +2,25 @@ var update_loop = setInterval(Main, 1000);
 Main();
 function Main(){
     var date = new Date();
-var now = new Date().getTime();
-var month = date.getMonth();
-var day = date.getDate();
-var year = date.getFullYear();
-var countDownDate = new Date(`Oct 31, ${year} 0:0:0`).getTime();
-var distance = countDownDate - now;
+    var now = new Date().getTime();
+    var month = date.getMonth();
+    var day = date.getDate();
+    var year = date.getFullYear();
+    var countDownDate = new Date(`Oct 31, ${year} 0:0:0`).getTime();
+    var distance = countDownDate - now;
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
- if (days > 45) return;
+ if (days > 45 || days < -7) return;
  document.getElementById("Spook").innerHTML = "<h1>" + days + "d " + hours + "h "
   + minutes + "m " + seconds + "s " + " till Halloween</h1>";
     if (month == 9 && day == 31) {
         document.getElementById("Spook").innerHTML = "<h1>It's Halloween!!!</h1>";
     } else if (distance < 0 || distance > -9) {
         document.getElementById("Spook").innerHTML = "<h1>Halloween is done.</h1>";
-    } else if (distance < -9) {
+    } else if (distance < -7) {
         document.getElementById("Spook").style.display = "none";
     }
 }
