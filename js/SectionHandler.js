9b5dea3589031b87066d8f6a59bdac04526649ad
Sajid <Sajidmonowar11@gmail.com>
2023-07-26 19:49:02 -0400

    ok spacing and disappearing fixed

--- js/SectionHandler.js
+++ js/SectionHandler.js
@@ -1,19 +1,31 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
-  
-  window.addEventListener("scroll", reveal);
+}
 
-  reveal();
+function scrollIndicate() {
+  let scrolldiv = document.querySelector("#scrollIndicator");
+  if (window.scrollY > 230) {
+    console.log(window.scrollY);
+    scrolldiv.style.opacity = 0;
+  } else {
+    scrolldiv.style.opacity = 1;
+  }
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", scrollIndicate);
+
+reveal();
+scrollIndicate();
