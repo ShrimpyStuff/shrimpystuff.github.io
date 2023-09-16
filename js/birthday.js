685d4c1ccfd8dab41cc2153847fa88340c19471c
Sajid <Sajidmonowar11@gmail.com>
2023-09-16 18:52:40 -0400

    Update some timers

--- js/birthday.js
+++ js/birthday.js
@@ -1,8 +1,11 @@
- var date = new Date();
-  var time = date.toLocaleString("en-CA", {timeZone: "America/Toronto"});
-  var day = date.getDate().toLocaleString("en-CA", {timeZone: "America/Toronto"});
-  var month = date.getMonth().toLocaleString("en-CA", {timeZone: "America/Toronto"});
+let date = new Date();
+let time = date.toLocaleString("en-CA", {timeZone: "America/Toronto"});
+let day = date.getDate().toLocaleString("en-CA", {timeZone: "America/Toronto"});
+let month = date.getMonth().toLocaleString("en-CA", {timeZone: "America/Toronto"});
+
+const main = document.getElementById("main_sect");
+
 if (day ==  17 && month == 9) {
-    Title.innerHTML = "IT'S MY BIRTHDAY | ShrimpyStuff";
-    document.title = "IT'S MY BIRTHDAY | ShrimpyStuff | Home";
-  }
+    main.innerHTML += "</br><h1 style=\"font-size:xx-large\"> IT'S MY BIRTHDAY </h1>";
+    document.title = "IT'S MY BIRTHDAY | ShrimpyStuff";
+}
--- js/spook.js
+++ js/spook.js
@@ -1,25 +1,27 @@
-var update_loop = setInterval(Main, 1000);
+let update_loop = setInterval(Main, 1000);
 Main();
-function Main(){
-    var date = new Date();
-    var now = new Date().getTime();
-    var month = date.getMonth();
-    var day = date.getDate();
-    var year = date.getFullYear();
-    var countDownDate = new Date(`Oct 31, ${year} 0:0:0`).getTime();
-    var distance = countDownDate - now;
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
- if (days > 45 || days < -7) {
-	clearInterval(update_loop);
-	document.getElementById("Spook").style.display = "none";
-	return;
- };
- document.getElementById("Spook").innerHTML = "<h1>" + days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s " + " till Halloween</h1>";
+function Main(){
+    let date = new Date();
+    let now = new Date().getTime();
+    let month = date.getMonth();
+    let day = date.getDate();
+    let year = date.getFullYear();
+    let countDownDate = new Date(`Oct 31, ${year} 0:0:0`).getTime();
+    let distance = countDownDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (days > 45 || days < -7) {
+        clearInterval(update_loop);
+        document.getElementById("Spook").style.display = "none";
+        return;
+    };
+
+    document.getElementById("Spook").innerHTML = "</br><h1>" + days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s " + " till Halloween</h1>";
     if (month == 9 && day == 31) {
         document.getElementById("Spook").innerHTML = "<h1>It's Halloween!!!</h1>";
     } else if (distance < 0) {
